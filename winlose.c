39d146d11c91fc102c405c65c4135c7e8c965e9a
DKC195 <Dhirajkc195@gmail.com>
2021-06-30 01:42:44 +0545

    Reached A playable stage.

--- winlose.c
+++ winlose.c
@@ -2,10 +2,11 @@
 
 int winlose() {
   for (int i=0; i<3; i++) {
-    if (a[i][1]==a[i][2]==a[i][3]) return 1;
+    if ((a[i][0]==a[i][1]) && (a[i][0]==a[i][2])) return 1;
   }
   for (int i=0; i<3; i++) {
-    if (a[1][i]==a[2][i]==a[3][i]) return 1;
+    if ((a[0][i]==a[1][i]) && (a[0][i]==a[2][i])) return 1;
   }
-  if (a[0][0]==a[1][1]==a[2][2] || a[0][2]==a[1][1]==a[2][0]) return 1;
+  if ((a[0][0]==a[1][1]) && (a[0][0]==a[2][2]) || (a[0][2]==a[1][1]) && (a[0][2]==a[2][0])) return 1;
+  else return 0;
 }
