d0417193e14ecfffbcbb1935e4e443a725c59a46
DKC195 <Dhirajkc195@gmail.com>
2021-06-29 11:30:41 +0545

    Updaed Output and final

--- final.c
+++ final.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 
 int outline(int, int, char);
+char a[3][3]={'A','B','C','D','E','F','G','H','I'};
 
 int main(void) {
-  printf(".................Tic-Tac-Toe.............\n");
-  char x,y,z,a[3][3]={'A','B','C','D','E','F','G','H','I'};
-  int i,j,n,b[9]={0,0,0,0,0,0,0,0,0};
-  printf("Users can type an alphabet as their symbol and \n can choose 1-9 places\n");
+  printf(".........Tic-Tac-Toe.........\n");
+  char sym_x, sym_y,sym,inp;
+  int x_axis,y_axis,n;
+  outline(1,1,'A');
+  printf("Users can type an alphabet as their symbol and can choose 1-9 places\n");
   printf("User 1 Symbol:- ");
-  scanf("%c", &x);
+  scanf("%c", &sym_x);
   printf("User 2 Symbol:- ");
-  scanf("%c", &y);
-  i=1;
-  j=1;
-  z='x';
-  outline(i,j,z);
+  scanf(" %c", &sym_y);
+  printf("Enter the position in Capital: ");
+  scanf(" %c", &inp);
+  for (int i=0; i<3; i++) {
+    for (int j=0; j<3; j++) {
+      if (inp==a[i][j]) {
+        x_axis=i;
+        y_axis=j;
+        break;
+      }
+    }
+  }
+  sym='x';
+  outline(x_axis,y_axis,sym);
 
   return 0;
 }
 
 int outline(int i, int j, char z) {
-  char a[3][3]={'A','B','C','D','E','F','G','H','I'};
-  a[i][j]='z';
+  a[i][j]=z;
   printf("\n      _________________\n");
   printf("     |     |     |     | \n");
   printf("     |  %c  |  %c  |  %c  | \n", a[0][0], a[0][1], a[0][2]);
--- outline.c
+++ outline.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 
-int main(void) {
-  printf(".................Tic-Tac-Toe.............\n");
+int outline(int i, int j, char z) {
   char a[3][3]={'A','B','C','D','E','F','G','H','I'};
-  printf("      _________________\n");
+  a[i][j]='z';
+  printf("\n      _________________\n");
   printf("     |     |     |     | \n");
   printf("     |  %c  |  %c  |  %c  | \n", a[0][0], a[0][1], a[0][2]);
   printf("     |_____|_____|_____| \n");
@@ -11,7 +10,7 @@ int main(void) {
   printf("     |  %c  |  %c  |  %c  | \n", a[1][0], a[1][1], a[1][2]);
   printf("     |_____|_____|_____| \n");
   printf("     |     |     |     | \n");
-  printf("     |  %c  |  %c  |  %c  | \n", a[2][0], a[1][1], a[2][2]);
+  printf("     |  %c  |  %c  |  %c  | \n", a[2][0], a[2][1], a[2][2]);
   printf("     |_____|_____|_____| \n");
   return 0;
 }
