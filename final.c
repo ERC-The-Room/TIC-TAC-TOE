9c5f9787f3a252d6a21f33d2c8f3d6f8b2e71b07
DKC195 <Dhirajkc195@gmail.com>
2021-06-29 11:55:42 +0545

    Created Funct and editd Outline and final.

--- final.c
+++ final.c
@@ -3,12 +3,14 @@
 int outline(int, int, char);
 char a[3][3]={'A','B','C','D','E','F','G','H','I'};
 
+#include "funct.h"
+
 int main(void) {
   printf(".........Tic-Tac-Toe.........\n");
   char sym_x, sym_y,sym,inp;
   int x_axis,y_axis,n;
-  outline(1,1,'A');
-  printf("Users can type an alphabet as their symbol and can choose 1-9 places\n");
+  outline(0,0,'A');
+  printf("\nUsers can type an alphabet as their symbol and can choose 1-9 places\n");
   printf("User 1 Symbol:- ");
   scanf("%c", &sym_x);
   printf("User 2 Symbol:- ");
@@ -26,21 +28,7 @@ int main(void) {
   }
   sym='x';
   outline(x_axis,y_axis,sym);
+  printf("%c", a[0][0]);
 
-  return 0;
-}
-
-int outline(int i, int j, char z) {
-  a[i][j]=z;
-  printf("\n      _________________\n");
-  printf("     |     |     |     | \n");
-  printf("     |  %c  |  %c  |  %c  | \n", a[0][0], a[0][1], a[0][2]);
-  printf("     |_____|_____|_____| \n");
-  printf("     |     |     |     | \n");
-  printf("     |  %c  |  %c  |  %c  | \n", a[1][0], a[1][1], a[1][2]);
-  printf("     |_____|_____|_____| \n");
-  printf("     |     |     |     | \n");
-  printf("     |  %c  |  %c  |  %c  | \n", a[2][0], a[2][1], a[2][2]);
-  printf("     |_____|_____|_____| \n");
   return 0;
 }
--- outline.c
+++ outline.c
@@ -1,7 +1,7 @@
+#include <stdio.h>
 
 int outline(int i, int j, char z) {
-  char a[3][3]={'A','B','C','D','E','F','G','H','I'};
-  a[i][j]='z';
+  a[i][j]=z;
   printf("\n      _________________\n");
   printf("     |     |     |     | \n");
   printf("     |  %c  |  %c  |  %c  | \n", a[0][0], a[0][1], a[0][2]);
