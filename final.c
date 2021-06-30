099e2d1646e58643f4b496fe353145dbcb880409
DKC195 <Dhirajkc195@gmail.com>
2021-06-30 10:34:17 +0545

    Added Verification Logics.

--- final.c
+++ final.c
@@ -3,23 +3,31 @@ char a[3][3]={'A','B','C','D','E','F','G','H','I'};
 #include "funct.h"
 
 int main(void) {
-  char usym[2]={'X','O'},inp;
-  int x_axis,y_axis,n=0,out;
+  char b[3][3]={'A','B','C','D','E','F','G','H','I'},usym[2]={'X','O'},inp;
+  int x_axis,y_axis,n=0,m,out;
   printf("                  .........Tic-Tac-Toe.........\n");
   outline(0,0,'A');
   printf("\nUsers can type an alphabet as their symbol and can choose 1-9 places\n X for User A and O for User B\n");
   turn:
   printf("Enter the position in Capital: ");
   scanf(" %c", &inp);
+  m=0;
   for (int i=0; i<3; i++) {
     for (int j=0; j<3; j++) {
-      if (inp==a[i][j]) {
+      if (inp==b[i][j]) {
         x_axis=i;
         y_axis=j;
-        if(verification(a[i][j])==1) goto turn;
+        if (verification(a[i][j])==1) goto turn;
         a[i][j]=usym[n%2];
         out=a[i][j];
         break;
+      }
+      else if (inp!=b[i][j]) {
+        m++;
+        if (m==8) {
+          printf("The position does not exist.\n");
+          goto turn;
+        }
       }
     }
   }
